11f65224871681c881af4359c04425fed0cea7d0
GitHub Action <action@github.com>
2025-10-29 11:25:58 +0000

    🔖 Update version to 0.6.0 for release v0.6.0

--- src/palabra_ai/__init__.py
+++ src/palabra_ai/__init__.py
@@ -90,7 +90,7 @@ from palabra_ai.task.adapter.device import (  # noqa: F401
 from palabra_ai.task.adapter.dummy import DummyReader, DummyWriter  # noqa: F401
 from palabra_ai.task.adapter.file import FileReader, FileWriter  # noqa: F401
 
-__version__ = "0.5.13"
+__version__ = "0.6.0"
 
 __all__ = [
     # Client
